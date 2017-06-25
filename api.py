496dcc417e58531bec93132862982eb8442b6e15
DaoGatech <stephenhdao@gatech.edu>
2017-06-25 18:44:57 -0500

    add sketch code

--- api.py
+++ api.py
@@ -1,14 +1,68 @@
-from flask import Flask
+from flask import Flask, render_template, Response, request, jsonify, json
 from flask_restful import Resource, Api
-
+from sketch import sketch, sharpen
+from flask_bootstrap import Bootstrap
+import cv2
 app = Flask(__name__)
+Bootstrap(app)
 api = Api(app)
+vc = None
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
 
-api.add_resource(HelloWorld, '/')
+def sketchWebcam():
+    global vc
+    if vc:
+        vc.release()
+        vc = None
+    vc = cv2.VideoCapture(0)
+    """Video streaming generator function."""
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite('t.jpg', sketch(frame))
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+
+def sharpenWebcam():
+    global vc
+    if vc:
+        print("here")
+        vc.release()
+        vc = None
+    vc = cv2.VideoCapture(0)
+    """Video streaming generator function."""
+    while True:
+        rval, frame = vc.read()
+        cv2.imwrite('t.jpg', sharpen(frame))
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
+
+@app.route('/webcam', methods=["GET", "POST"])
+def webcam():
+    """Video streaming route. Put this in the src attribute of an img tag."""
+    if request.method == 'GET':
+        return Response(sketchWebcam(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    if request.content_type != 'application/json':
+        return jsonify({})
+    try:
+        webcamMode = json.loads(request.data)
+        webcamModeMap = {
+            'sketch': sketchWebcam,
+            'sharpen': sharpenWebcam
+        }
+        webcamFunc = webcamModeMap[webcamMode['mode']]      
+        return Response(sharpenWebcam(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+    except ValueError as e:
+        print(e)
+        return jsonify({})
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ sketch.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+
+#kernel sharpening
+kernel_sharpening = np.array([[-1, -1, -1], [-1, 9, -1], [-1, -1, -1]])
+
+def sketch(image):
+
+    img_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    img_gray_blur = cv2.GaussianBlur(img_gray, (5,5), 0)
+    canny_edges = cv2.Canny(img_gray_blur, 10, 70)
+    ret, mask = cv2.threshold(canny_edges, 70, 255, cv2.THRESH_BINARY_INV)
+    return mask
+
+def sharpen(image):
+    return cv2.filter2D(image, -1, kernel_sharpening)
