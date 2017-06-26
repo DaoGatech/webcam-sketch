8f68e205d308daffab7f97c4e20169355bfb9c8b
DaoGatech <stephenhdao@gatech.edu>
2017-06-25 19:31:31 -0500

    add functionality

--- api.py
+++ api.py
@@ -3,10 +3,12 @@ from flask_restful import Resource, Api
 from sketch import sketch, sharpen
 from flask_bootstrap import Bootstrap
 import cv2
+import time
 app = Flask(__name__)
 Bootstrap(app)
 api = Api(app)
-vc = None
+webcamFunc = None
+
 
 @app.route('/')
 def index():
@@ -14,36 +16,26 @@ def index():
     return render_template('index.html')
 
 def sketchWebcam():
-    global vc
-    if vc:
-        vc.release()
-        vc = None
+    global webcamFunc
     vc = cv2.VideoCapture(0)
+    t_end = time.time() + 5
     """Video streaming generator function."""
-    while True:
+    while time.time() < t_end:
         rval, frame = vc.read()
-        cv2.imwrite('t.jpg', sketch(frame))
+        cv2.imwrite('t.jpg', webcamFunc(frame))
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
 
-def sharpenWebcam():
-    global vc
-    if vc:
-        print("here")
-        vc.release()
-        vc = None
-    vc = cv2.VideoCapture(0)
-    """Video streaming generator function."""
-    while True:
-        rval, frame = vc.read()
-        cv2.imwrite('t.jpg', sharpen(frame))
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
 
 @app.route('/webcam', methods=["GET", "POST"])
 def webcam():
+    global webcamFunc
+    global t_end
     """Video streaming route. Put this in the src attribute of an img tag."""
     if request.method == 'GET':
+        if not webcamFunc:
+            webcamFunc = sketch
+
         return Response(sketchWebcam(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
@@ -52,11 +44,12 @@ def webcam():
     try:
         webcamMode = json.loads(request.data)
         webcamModeMap = {
-            'sketch': sketchWebcam,
-            'sharpen': sharpenWebcam
+            'sketch': sketch,
+            'sharpen': sharpen
         }
-        webcamFunc = webcamModeMap[webcamMode['mode']]      
-        return Response(sharpenWebcam(),
+        webcamFunc = webcamModeMap[webcamMode['mode']]
+              
+        return Response(sketchWebcam(),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
 
     except ValueError as e:
